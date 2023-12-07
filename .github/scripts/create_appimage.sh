c560e3366d3015ae22c765162988bc5d7e28d8cd
justaCasualCoder <120528794+justaCasualCoder@users.noreply.github.com>
2023-12-07 15:24:49 -0500

    Update create_appimage.sh

--- .github/scripts/create_appimage.sh
+++ .github/scripts/create_appimage.sh
@@ -35,7 +35,7 @@ EOF
 
 echo "Installing LibFuse"
 sudo add-apt-repository universe
-sudo apt install libfuse2
+sudo apt install libfuse2 libsdl2
 echo "making appdir"
 ./linuxdeploy-x86_64.AppImage --appdir scrcpy_dir 1> /dev/null
 
